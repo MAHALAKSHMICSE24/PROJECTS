6f74277e7d2f16b30468c8c955c659fb38533366
Mahalakshmi M <113227568+MAHALAKSHMICSE24@users.noreply.github.com>
2022-10-10 20:57:40 +0530

    Add files via upload

---
+++ Mini-Project/basics/circle.py
@@ -0,0 +1,6 @@
+import turtle 
+tr = turtle.Turtle()
+rad = 80
+tr.circle(rad)
+turtle.clear()
+turtle.done()
---
+++ Mini-Project/basics/star.py
@@ -0,0 +1,7 @@
+import turtle 
+tr = turtle.Turtle()
+for i in range(5):
+    tr.forward(80)
+    tr.right(144)
+turtle.clear()
+turtle.done()
