827b3b438ae4f6057de2f476e46c72d4224136aa
Mahalakshmi M <113227568+MAHALAKSHMICSE24@users.noreply.github.com>
2022-10-10 20:50:19 +0530

    Delete basics directory

--- basics/circle.py
+++
@@ -1,6 +0,0 @@
-import turtle 
-tr = turtle.Turtle()
-rad = 80
-tr.circle(rad)
-turtle.clear()
-turtle.done()
--- basics/star.py
+++
@@ -1,7 +0,0 @@
-import turtle 
-tr = turtle.Turtle()
-for i in range(5):
-    tr.forward(80)
-    tr.right(144)
-turtle.clear()
-turtle.done()
